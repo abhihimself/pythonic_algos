25e53395e72f2eb556ee71d50100c4337dd2f9de
abhihimself <abhihimself@gmail.com>
2017-02-01 16:54:57 +0530

    An basic bubble sort and  its test cases

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -2,6 +2,8 @@ def get_list(input_list):
     return bubble_sorted_list(input_list)
 
 def bubble_sorted_list(input_list):
+    # for any n numbers there will be n-1 pairs
+    #Thats why we will run the loop n-1 to 0
     for i in range(len(input_list)-1,0,-1):
         for j in range(i):
             if input_list[j]>input_list[j+1]:
--- sorting/test_bubble_sort.py
+++ sorting/test_bubble_sort.py
@@ -3,14 +3,14 @@ import unittest
 
 class test_bubble(unittest.TestCase):
     def setUp(self):
-        self.test_list1 = [5, 4, 3, 2, 1]
+        self.test_list1 = [3, 2, 1]
         self.test_list2 = [1, 2, 3, 4, 5]
         self.test_list3 = [55, 89, 22, 34, 10000, 74]
 
 
     def test_reverse_list(self):
         out_list=bubble_sort.get_list(self.test_list1)
-        self.assertListEqual(out_list,[1,2,3,4,5])
+        self.assertListEqual(out_list,[1,2,3])
 
     def test_random_list(self):
         out_list=bubble_sort.get_list(self.test_list3)
